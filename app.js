6b9b8c0c8f7195af4894381ebaa390fe953ffcd3
umayer10-AI <mdumayer10@gmail.com>
2026-05-24 10:11:50 +0600

    add

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Car {
+    Brand;
+    Color;
+    Price;
+    Free;
+    constructor(Brand, Color, Price, Free) {
+        this.Brand = Brand;
+        this.Color = Color;
+        this.Price = Price;
+        this.Free = Free;
+    }
+}
+const b = new Car("TOYOTA", "Black", 5000, true);
+console.log(b.Brand)
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-let a = 10
+class Car {
+    constructor(public Brand: string,public Color: string,public Price: number,public Free: Boolean){
+
+    }
+}
+const b = new Car("TOYOTA", "Black", 5000, true)
